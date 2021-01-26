dad66073a21ea8da5972f754fffbd11ea7fa0c0d
Emerson Reis <emersonreisdacosta@gmail.com>
2021-01-26 16:21:29 -0400

    Cabecalho centralizado

--- src/components/cabecalho/index.js
+++ src/components/cabecalho/index.js
@@ -7,28 +7,23 @@ import './Cabecalho.css';
 function Cabecalho(){
     return(
         
-        <header className="header" >
-            <nav>
-                
+        <header className="layout-cabecalho">
+            <nav className="nav-menu">
                 <ul className="menu__cabecalho">
                     <li className="menu__item"> 
                         <a href ="index.html">Inicio</a>
                     </li>
-                    
                     <li className="menu__item">
                         <a href="sobre.html">Sobre</a>
                     </li>
-                    
                     <li className="menu__item">
                         <a href="index.html">
                             <img className="menu__logo" src={Logo} alt="Logo menina-cientista"/>
                         </a>
                     </li>
-                    
                     <li className="menu__item">
                         <a href="videos.html">Videos</a>
                     </li>
-                    
                     <li className="menu__item">
                         <a href="contato.html">Contato</a>
                     </li>
